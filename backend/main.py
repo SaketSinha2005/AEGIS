7c72c54a1327ef9356eab3a84523bd038e327081
Saket Kumar Sinha <ss691@snu.edu.in>
2026-07-19 07:03:01 +0530

    Replace local execution with Docker sandbox

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from generator import generate_code
 from pydantic import BaseModel
 import tempfile
 from pathlib import Path
 import subprocess
-import sys
 
 app = FastAPI(title="Code Executor")
 
@@ -22,27 +21,49 @@ class ExecuteResponse(BaseModel):
 @app.post("/execute")
 def execute(request: ExecuteRequest):
 
-    problem = request.problem
+    try:
+        problem = request.problem
 
-    result = generate_code(problem)
-    code = result["generated_code"]
+        result = generate_code(problem)
+        code = result["generated_code"]
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        solution_path = Path(tmpdir) / "solution.py"
-        solution_path.write_text(code)
-        print(solution_path)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            solution_path = Path(tmpdir) / "solution.py"
+            solution_path.write_text(code)
 
 
-        process = subprocess.run(
-            [sys.executable, str(solution_path)],
-            capture_output=True,
-            text=True,
-            timeout=10
+            process = subprocess.run(
+                [
+                    "docker",
+                    "run",
+                    "--rm",
+                    "--network", "none",
+                    "--cpus", "0.5",
+                    "--memory", "256m",
+                    "-v", f"{tmpdir}:/home/sandboxuser",
+                    "img-backend"
+                ],
+                capture_output=True,
+                text=True,
+                timeout=15
+            )
+
+            return ExecuteResponse(
+                generated_code=code,
+                stdout=process.stdout,
+                stderr=process.stderr,
+                exit_code=process.returncode
+            )
+        
+
+    except subprocess.TimeoutExpired:
+        raise HTTPException(
+            status_code=408,
+            detail="Execution timed out."
         )
 
-        return ExecuteResponse(
-            generated_code=code,
-            stdout=process.stdout,
-            stderr=process.stderr,
-            exit_code=process.returncode
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
         )
